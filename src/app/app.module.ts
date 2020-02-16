583e8853e825f16d4403a63d72f5261ff16edde7
Yousha Rizvi <yousharizvi@gmail.com>
2020-02-16 17:16:22 +0500

    WIP: Page layout setup and some other tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { HeaderComponent } from './layout';
+import { FooterComponent } from './layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleListComponent,
-    ArticleDetailComponent
+    ArticleDetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
